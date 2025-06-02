2217e1ec2c4d5df6c6716657377fdc928b15b2db
Zhaleh <rhmi.zhle@gmail.com>
2025-06-02 17:46:59 -0400

    updated forecasting

--- forecasting/arima_model.py
+++ forecasting/arima_model.py
@@ -1,22 +1,19 @@
 from statsmodels.tsa.arima.model import ARIMA
-import numpy as np
 from forecasting.base_model import BaseForecastModel
-import pandas as pd
+
 
 class ARIMAModel(BaseForecastModel):
-    def __init__(self, order=(5,1,0)):
+    def __init__(self, order=(1,0,0)):
         self.order = order
-        self.model = None
-        self.fitted_model = None
+        self.model_fit = None
 
     def fit(self, data, features, target):
-        # Assume 'target' is a univariate time series in order
-        # ARIMA works on a 1D series, so we only use target column
-        ts = data[target].values
-        self.model = ARIMA(ts, order=self.order)
-        self.fitted_model = self.model.fit()
+        y = data[target]
+        exog = data[features] if features else None
+        model = ARIMA(y, exog=exog, order=self.order, trend= [1,0,0,0])
+        self.model_fit = model.fit()
 
     def predict(self, data, features):
-        n = len(data)
-        preds = self.fitted_model.predict(start=0, end=n-1)
+        exog = data[features] if features else None
+        preds = self.model_fit.predict(start=0, end=len(data)-1, exog=exog)
         return preds
--- forecasting/cluster_forecasting_model.py
+++ forecasting/cluster_forecasting_model.py
@@ -1,9 +1,9 @@
-# forecasting/cluster_forecasting_model.py
-from collections import defaultdict
 import numpy as np
+import pandas as pd
+import copy
 
 class ClusterForecastingModel:
-    def __init__(self, cluster_model, base_model_cls, n_clusters=3):
+    def __init__(self, cluster_model, base_model_cls, n_clusters=4):
         self.cluster_model = cluster_model
         self.base_model_cls = base_model_cls
         self.models_by_cluster = {}
@@ -16,19 +16,29 @@ class ClusterForecastingModel:
         # Train one model per cluster
         for cluster in range(self.n_clusters):
             cluster_data = clustered_data[clustered_data['cluster'] == cluster]
-            model = self.base_model_cls()
-            model.fit(cluster_data, features, target)
+            if len(cluster_data) == 0:
+                continue
+            model = copy.deepcopy(self.base_model_cls)
+            model.fit(data = cluster_data, features=features, target=target)
             self.models_by_cluster[cluster] = model
 
     def predict(self, test_data, features):
-        # Assign clusters to test data
         clustered_data = self.cluster_model.predict(test_data.copy(), features)
 
-        # Predict per cluster
-        predictions = np.full(len(test_data), np.nan)
+        # Create predictions series with the same index
+        predictions = pd.Series(index=test_data.index, dtype=float)
+
         for cluster, model in self.models_by_cluster.items():
             cluster_idx = clustered_data[clustered_data['cluster'] == cluster].index
-            if len(cluster_idx) > 0:
-                preds = model.predict(clustered_data.loc[cluster_idx], features)
-                predictions[cluster_idx] = preds
+            if len(cluster_idx) == 0:
+                continue
+            subset = test_data.loc[cluster_idx]
+            preds = model.predict(subset[features], features)
+            predictions.loc[cluster_idx] = preds
+
+        # Handle missing predictions if any
+        if predictions.isnull().any():
+            print("Warning: some predictions missing, filling with mean.")
+            predictions.fillna(predictions.mean(), inplace=True)
+
         return predictions
